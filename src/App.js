e965874d8dd2f0863e94d1e2f21df022c82d86d1
seyifagun <seyifagun@gmail.com>
2022-07-20 18:38:26 +0100

    Remove redundant files

--- src/App.js
+++ src/App.js
@@ -1,45 +1,26 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from "react";
 import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import particlesOptions from "./particles.json";
 
 function App() {
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
 
-    return (
-        <div className="App">
-            <Particles options={particlesOptions} init={particlesInit}/>
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <p>
-                    Edit <code>src/particles.json</code> to customize Particles, then save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                <a
-                    className="App-link"
-                    href="https://particles.js.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    See Particles samples
-                </a>
-            </header>
+  return (
+    <>
+      <section id="header">
+        <div className="container header">
+            <h1>Hello</h1>
         </div>
-    );
+      </section>
+      <section>
+        <Particles options={particlesOptions} init={particlesInit} />
+      </section>
+    </>
+  );
 }
 
 export default App;
